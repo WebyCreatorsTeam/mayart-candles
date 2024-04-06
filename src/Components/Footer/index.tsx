1e8167aa1c6aa61040a60af813ceb5d18ab56248
Daniel Elias <danl3654321@gmail.com>
2024-04-06 21:28:24 +0300

    footer complited

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -4,17 +4,21 @@ import TelBtn from "./TelBtn";
 
 const index = () => {
   return (
-    <div className="flex w-full flex-col items-center gap-9 bg-red-200 px-4 py-11 text-lg font-normal sm:text-4xl">
-      <img src={"/images/ellipse15.svg"} alt="ellipse15" id="image2" className="hidden lg:flex"/>
+    <div className="flex w-full flex-row justify-center items-center gap-9 bg-red-200 px-4 py-11 text-lg font-normal sm:text-4xl">
 
-      <h3 className="lg:text-[32px]">!בואו נשמור על קשר</h3>
+
+      <img src={"/images/ellipse15.svg"} alt="ellipse15" id="image2" className="hidden lg:block w-[577px] h-[591px]"/>
+      <div className="!flex !flex-col items-center">
+      <h3 className="flex text-[32px]">!בואו נשמור על קשר</h3><br />
       <TelBtn />
-      <div className="flex gap-1">
+<div className="flex flex-row">
+ 
+      <div className="flex flex-row gap-1">
         <IconBtn href={"https://instagram.com"}>
           <img
             src={"/icons/instagram-icon.svg"}
             alt="instagram-icon"
-            className="sm:h-9 sm:w-9"
+            className="sm:h-9 sm:w-9 lg:h-9 lg:w-9"
           />
           Instagram
         </IconBtn>
@@ -22,7 +26,7 @@ const index = () => {
           <img
             src={"/icons/whatsapp-icon.svg"}
             alt="whatsapp-icon"
-            className="h-5 w-5 sm:h-9 sm:w-9"
+            className="h-5 w-5 sm:h-9 sm:w-9 lg:h-9 lg:w-9"
           />
           Whatsapp
         </IconBtn>
@@ -34,13 +38,17 @@ const index = () => {
           />
           Email
         </IconBtn>
+  
+</div>
       </div>
       <p className="text-xs sm:text-2xl lg:text-[32px]">
         created by <a href="https://weby.team/">WEBY</a>
       </p>
+</div>
+      <img src={"/images/ellipse16.svg"} alt="ellipse16" className="hidden lg:block w-[713px] h-[575px]"  />
 
-      <img src={"/images/ellipse16.svg"} alt="ellipse16" className="hidden lg:flex"  />
     </div>
+
   );
 };
 
