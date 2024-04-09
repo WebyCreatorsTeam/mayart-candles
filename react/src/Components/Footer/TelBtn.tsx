20947ea038efabff1abe317cdf3dfb3c7e0ca006
Etan Joseph Heyman <etan@heyman.net>
2024-04-09 14:27:18 -0500

    Restore footer changes before breaking "merge"

--- react/src/Components/Footer/TelBtn.tsx
+++ react/src/Components/Footer/TelBtn.tsx
@@ -3,16 +3,16 @@ import React from "react";
 const TelBtn = () => {
   return (
     <a
-      className="flex items-center gap-4 rounded-full bg-primary-pink px-[60px] py-[15px] align-middle text-2xl font-semibold text-white hover:bg-secondary-pink focus:bg-secondary-pink sm:gap-5 sm:px-16 sm:py-3 sm:text-3xl md:px-32 md:py-7 md:text-[58.86px]"
-      href="tel:+972 50-8122000"
-    >
-      <img
-        src={"/icons/phone-icon.svg"}
-        alt="phone-icon"
-        className="sm:h-12 sm:w-12 md:h-16 md:w-16"
-      />
-      050-8122000
-    </a>
+    className="flex items-center gap-4 rounded-full bg-primary-pink px-[60px] py-[15px] align-middle text-2xl font-semibold text-white hover:bg-secondary-pink focus:bg-secondary-pink sm:gap-5 sm:px-16 sm:py-3 sm:text-3xl md:px-34 md:py-7 md:text-[58.86px] lg:h-[73px] lg:w-[379px] lg:text-3xl lg:text-center lg:items-center"
+    href="tel:+972 50-8122000"
+  >
+    <img
+      src={"/icons/phone-icon.svg"}
+      alt="phone-icon"
+      className="sm:h-12 sm:w-12 md:h-16 md:w-16 lg:h-11 lg:w-11"
+    />
+    050-8122000
+  </a>
   );
 };
 
--- react/src/Components/Footer/index.tsx
+++ react/src/Components/Footer/index.tsx
@@ -4,42 +4,54 @@ import TelBtn from "./TelBtn";
 
 const index = () => {
   return (
-    <div className="flex w-full flex-col items-center gap-9 bg-red-200 px-4 py-11 text-lg font-normal sm:text-4xl">
-      {/* <img src={"/images/ellipse15.svg"} alt="ellipse15" id="image2" /> */}
-
-      <h3 className="">!בואו נשמור על קשר</h3>
-      <TelBtn />
-      <div className="flex gap-1">
-        <IconBtn href={"https://instagram.com"}>
-          <img
-            src={"/icons/instagram-icon.svg"}
-            alt="instagram-icon"
-            className="sm:h-9 sm:w-9 hover:stroke-2"
-          />
-          Instagram
-        </IconBtn>
-        <IconBtn href="https://whatsapp.com">
-          <img
-            src={"/icons/whatsapp-icon.svg"}
-            alt="whatsapp-icon"
-            className="h-5 w-5 sm:h-9 sm:w-9 hover:stroke-2"
-          />
-          Whatsapp
-        </IconBtn>
-        <IconBtn href="https://gmail.com">
-          <img
-            src={"/icons/mail-icon.svg"}
-            alt="mail-icon"
-            className="h-5 w-5 sm:h-9 sm:w-9 hover:stroke-2"
-          />
-          Email
-        </IconBtn>
+    <div className="flex w-full flex-row items-center justify-center gap-9 bg-red-200 px-4 py-11 text-lg font-normal sm:text-4xl">
+      <img
+        src={"/images/ellipse15.svg"}
+        alt="ellipse15"
+        id="image2"
+        className="hidden h-[591px] w-[577px] lg:block"
+      />
+      <div className="!flex !flex-col items-center">
+        <h3 className="flex text-[32px]">!בואו נשמור על קשר</h3>
+        <br />
+        <TelBtn />
+        <div className="flex flex-row">
+          <div className="flex flex-row gap-1">
+            <IconBtn href={"https://instagram.com"}>
+              <img
+                src={"/icons/instagram-icon.svg"}
+                alt="instagram-icon"
+                className="sm:h-9 sm:w-9 lg:h-9 lg:w-9"
+              />
+              Instagram
+            </IconBtn>
+            <IconBtn href="https://whatsapp.com">
+              <img
+                src={"/icons/whatsapp-icon.svg"}
+                alt="whatsapp-icon"
+                className="h-5 w-5 sm:h-9 sm:w-9 lg:h-9 lg:w-9"
+              />
+              Whatsapp
+            </IconBtn>
+            <IconBtn href="https://gmail.com">
+              <img
+                src={"/icons/mail-icon.svg"}
+                alt="mail-icon"
+                className="h-5 w-5 sm:h-9 sm:w-9"
+              />
+              Email
+            </IconBtn>
+          </div>
+        </div>
+        <p className="text-xs sm:text-2xl lg:text-[32px]">
+          created by <a href="https://weby.team/">WEBY</a>
+        </p>
       </div>
-      <p className="text-xs sm:text-2xl">
-        created by <a href="https://weby.team/">WEBY</a>
-      </p>
-
-      {/* <img src={"/images/ellipse16.svg"} alt="ellipse16" id="image1" /> */}
+      <img
+        src={"/images/ellipse16.svg"}
+        alt="ellipse16"
+        className="hidden h-[575px] w-[713px] lg:block"
+      />
     </div>
   );
 };
