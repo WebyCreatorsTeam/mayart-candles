2dd51c4a712a4fc289b9b62d2772b0cd84002039
Etan Joseph Heyman <etan@heyman.net>
2024-05-11 16:56:54 -0500

    Add candle type

---
+++ sharedData/candles.ts
@@ -0,0 +1,22 @@
+type CandleType = {
+  name: string;
+  shape: string;
+  colors: string[];
+  fragrances: string[];
+  price: number;
+  salePrice: number;
+  pictures: string[];
+  description: string;
+};
+const candles: CandleType[] = [
+  {
+    name: 'נר בועות',
+    shape: 'קוביה',
+    colors: ['ורוד', 'ירוק', 'לבן', 'שחור'],
+    fragrances: ['וניל', 'ורדים', 'לבנדר'],
+    price: 40,
+    salePrice: 20,
+    pictures: ['https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com%2Fsphere-candles%2Fs%3Fk%3Dsphere%2Bcandles&psig=AOvVaw22cjAD8ii66eMog3FxDHyc&ust=1715549848076000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKiqnKzHhoYDFQAAAAAdAAAAABAE', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com%2Fsphere-candles%2Fs%3Fk%3Dsphere%2Bcandles&psig=AOvVaw22cjAD8ii66eMog3FxDHyc&ust=1715549848076000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKiqnKzHhoYDFQAAAAAdAAAAABAE'],
+    description: 'נר בועות גודל xxxxxx מיוצר משעוות יד טבעית עבודת יד'
+  }
+];
