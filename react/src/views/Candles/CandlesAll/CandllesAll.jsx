676124c30c9f244dd49589627700e0551c2b2dce
Daniel Elias <danl3654321@gmail.com>
2024-05-22 22:14:24 +0300

    fix responsive all screen calends all

--- react/src/views/Candles/CandlesAll/CandllesAll.jsx
+++ react/src/views/Candles/CandlesAll/CandllesAll.jsx
@@ -18,15 +18,15 @@ const CandllesAll = () => {
 
   return (
     <div className='flex flex-col mt-40 w-[100%]'>
-        <p className='ml-[41%] mt-8 text-[64px] font-normal'>כל הנרות</p>
+        <p className='ml-[32%] mt-8 text-[32px] lg:ml-[41%] lg:text-[64px] font-normal'>כל הנרות</p>
     <div className='flex justify-center items-center mt-6 flex-wrap'>
       {dataCandlles.map((item, i) => (
         <div key={i} className='flex flex-col items-center'> 
-          <img src={item.pictures} alt="calends" className='flex w-80 h-96 p-3 gap-2' />
-          <h1 className='text-2xl font-normal'>{item.name}</h1>
+          <img src={item.pictures} alt="calends" className='flex w-[50vw] h-[40vh] lg:w-[30vw] lg:h-[75vh] p-4 gap-2' />
+          <h1 className='text-[16px] lg:text-[30px] font-normal tracking-normal'>{item.name}</h1>
           <div className='flex flex-row gap-2 justify-evenly'>
-          <p className='text-xl'>₪{item.salePrice}</p>
-          <span className='text-xl line-through text-black/[.50]'>₪{item.price}</span>
+          <p className='text-[16px] lg:text-[32px] '>₪{item.salePrice}</p>
+          <span className='text-[16px] lg:text-[32px] line-through text-black/[.50]'>₪{item.price}</span>
           </div>
         </div>
       ))}
