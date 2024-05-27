67ab2643c3c94cf178d3f4ddd2d9ecb97391c805
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-05-27 21:00:55 +0300

    check route

--- server/controllers/candles.ts
+++ server/controllers/candles.ts
@@ -7,3 +7,10 @@ export const getAllCandles = (req: Request, res: Response) => {
         return res.send(error)
     }
 }
+export const getOneCandle = (req: Request, res: Response) => {
+    try {
+        return res.send("One Candle")
+    } catch (error) {
+        return res.send(error)
+    }
+}
--- server/routers/candles/candles.ts
+++ server/routers/candles/candles.ts
@@ -1,7 +1,8 @@
 import { Request, Response, Router } from "express";
-import { getAllCandles } from "../../controllers/candles"
+import { getAllCandles, getOneCandle } from "../../controllers/candles"
 const router = Router()
 
 router.get('/get-all', getAllCandles)
+router.get('/get-one', getOneCandle)
 
 export default router
