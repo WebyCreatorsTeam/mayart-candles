02aef85f454279a097ea1cf35063341947000f19
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-05-27 22:30:17 +0300

    candle
    add candle, created model

--- server/controllers/candles.controller.ts
+++ server/controllers/candles.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { Candle } from '../model/candle.model'
 
 export const getAllCandles = (req: Request, res: Response) => {
     try {
@@ -7,9 +8,22 @@ export const getAllCandles = (req: Request, res: Response) => {
         return res.send(error)
     }
 }
+
 export const getOneCandle = (req: Request, res: Response) => {
     try {
         return res.send("One Candle")
+    } catch (error) {
+        return res.send(error)
+    }
+}
+
+export const addCandle = async (req: Request, res: Response) => {
+    try {
+        const data = req.body
+        console.log(data)
+        const newCandle = new Candle(data)
+        await newCandle.save()
+        return res.status(200).json({ text: "candle saved" })
     } catch (error) {
         return res.send(error)
     }
--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,12 @@
+import 'dotenv/config'
 import express, { Request, Response } from 'express'
 import candlesRout from './routers/candles/candles'
-
+import { dbconnect } from './DBconnect/dbconnect'
 const app = express()
 const PORT = process.env.PORT || 7575
+
+dbconnect()
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     try {
