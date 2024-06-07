3a6b6ebb5f023d536a31193f9575f7d1a5b6b726
Etan Joseph Heyman <etan@heyman.net>
2024-06-07 11:09:40 -0500

    Remove homepage navbar, fix menu social links

--- react/src/Components/NavBar/SideNavBar/SideNavBarMenu.tsx
+++ react/src/Components/NavBar/SideNavBar/SideNavBarMenu.tsx
@@ -4,12 +4,10 @@ import CandlesBySizeMenu from "./CandlesBySizeMenu";
 import NavBarMenuSocials from "./SideNavBarMenuSocials";
 
 const SideNavBarMenu = ({
-  homepage,
   closeMenus,
   sizeMenuIsOpen,
   toggleSizeMenu,
 }: {
-  homepage?: boolean;
   closeMenus?: () => void;
   sizeMenuIsOpen: boolean;
   toggleSizeMenu: () => void;
@@ -17,45 +15,28 @@ const SideNavBarMenu = ({
   return (
     <nav
       dir="rtl"
-      className={`flex flex-col items-start divide-y-[1px] border-[[#B0C4B1]/30] text-center lg:items-center lg:pt-20 ${homepage ? "py-12 font-semibold sm:text-[40px] sm:leading-[53.2px] lg:hidden" : ""}`}
+      className={`flex flex-col items-start divide-y-[1px] border-[[#B0C4B1]/30] text-center lg:items-center lg:pt-20`}
     >
-      {homepage ? null : (
-        <MenuNavLink onClick={closeMenus} to="/" text="ראשי" />
-      )}
+      <MenuNavLink onClick={closeMenus} to="/" text="ראשי" />
+      <MenuNavLink onClick={closeMenus} to="/candles" text="כל הנרות" />
       <MenuNavLink
-        className={homepage ? "text-xl sm:text-[40px] sm:leading-[53.2px]" : ""}
-        onClick={closeMenus}
-        to="/candles"
-        text="כל הנרות"
-      />
-      <MenuNavLink
-        className={homepage ? "text-xl sm:text-[40px] sm:leading-[53.2px]" : ""}
         onClick={closeMenus}
         to="/candles/designed"
         text="נרות מעוצבים"
       />
       <CandlesBySizeMenu
-        className={homepage ? "text-xl sm:text-[40px] sm:leading-[53.2px]" : ""}
-        childrenClassName={
-          homepage ? "text-xl sm:text-[40px] sm:leading-[53.2px]" : ""
-        }
         closeMenus={closeMenus || toggleSizeMenu}
         sizeMenuIsOpen={sizeMenuIsOpen}
         toggleSizeMenu={toggleSizeMenu}
       />
       <MenuNavLink
-        className={homepage ? "text-xl sm:text-[40px] sm:leading-[53.2px]" : ""}
         onClick={closeMenus}
         to="/candles/in-vessel"
         text="נרות בכלי"
       />
-      {homepage ? null : (
-        <MenuNavLink onClick={closeMenus} to="/about" text="אודות" />
-      )}
-      {homepage ? null : (
-        <MenuNavLink onClick={closeMenus} to="/contact" text="צור קשר" />
-      )}
-      {homepage ? null : <NavBarMenuSocials />}
+      <MenuNavLink onClick={closeMenus} to="/about" text="אודות" />
+      <MenuNavLink onClick={closeMenus} to="/contact" text="צור קשר" />
+      <NavBarMenuSocials />
     </nav>
   );
 };
