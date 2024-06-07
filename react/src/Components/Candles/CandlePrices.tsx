e361c47e3783ec7d0e69309f80fffce8bd6838de
Etan Joseph Heyman <etan@heyman.net>
2024-06-07 13:13:00 -0500

    Add CandlePrices component

--- react/src/Components/Candles/CandlePrices.tsx
+++ react/src/Components/Candles/CandlePrices.tsx
@@ -1,5 +1,4 @@
 import { CandleType } from "../../utils/types/candles";
-
 const CandlePrices = ({ currentCandle }: { currentCandle: CandleType }) => {
   return (
     <span className="flex flex-row-reverse gap-2 text-base font-normal sm:gap-4 sm:text-[34.22px] sm:leading-[45.51px]">
