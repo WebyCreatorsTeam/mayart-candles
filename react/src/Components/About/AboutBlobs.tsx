f3dbb6122c264b3edcda500da4b46c5f4093d279
Etan Joseph Heyman <etan@heyman.net>
2024-06-09 10:54:45 -0500

    Oversized blobs on desktop - otherwise done.

--- react/src/Components/About/AboutBlobs.tsx
+++ react/src/Components/About/AboutBlobs.tsx
@@ -4,13 +4,23 @@ const AboutBlobs = () => {
   return (
     <>
       <img
-        className="absolute -left-[88px] -top-[11px] -z-50 h-[406px] w-[304px] sm:top-[76.51px] sm:h-[868.22px] sm:w-[650.09px]"
+        className="absolute -left-[88px] -top-[11px] -z-50 h-[406px] w-[304px] sm:top-[76.51px] sm:h-[868.22px] sm:w-[650.09px] xl:hidden"
         src="/images/ellipse18.svg"
         alt=""
       />
       <img
-        className="absolute -right-[143.5px] top-60 -z-50 h-[394.16px] w-[334.17px] sm:top-[604.37px] sm:h-[842.89px] sm:w-[714.62px]"
+        className="absolute -right-[143.5px] top-60 -z-50 h-[394.16px] w-[334.17px] sm:top-[604.37px] sm:h-[842.89px] sm:w-[714.62px] xl:hidden"
         src="/images/ellipse17.svg"
+        alt=""
+      />
+      <img
+        className="absolute  top-40 left-0 -z-50 hidden w-[1136.5px] h-[1085.5px] xl:block"
+        src="/images/ellipse19.svg"
+        alt=""
+      />
+      <img
+        className="absolute right-0 -top-[271.5px] -z-50 hidden w-[928px] h-[844px] xl:block"
+        src="/images/ellipse20.svg"
         alt=""
       />
     </>
--- react/src/Components/About/AboutImages.tsx
+++ react/src/Components/About/AboutImages.tsx
@@ -7,10 +7,15 @@ const AboutImages = ({
   images: aboutLoaderInformation["images"];
 }) => {
   return (
-    <div className="hidden lg:flex">
-      {images.map((image, index) => (
-        <img className="basis-1/3" key={index} src={image} />
-      ))}
+    <div className="hidden text-[32px] leading-[42.56px] xl:flex gap-24 flex-col">
+      מאחורי הקלעים של MAYART...
+      <div className="justify-center gap-[47px] px-28 xl:flex">
+        {images.map((image, index) => (
+          <div key={index} className="h-fit min-w-[400px]">
+            <img className="object-cover" alt={`candle ${index}`} src={image} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
