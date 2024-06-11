679418c07d8429e9ce7b19373b0bcef26bc3cd03
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-06-11 13:44:20 +0300

    style-dasshboard-fix

--- react/src/views/Dashboard/Categories/CategoryAdd.tsx
+++ react/src/views/Dashboard/Categories/CategoryAdd.tsx
@@ -1,69 +1,57 @@
-import axios from "axios";
-import React, { FC, useState } from "react";
-import { ICategories } from "./CategoriesDashboard";
+import axios from 'axios'
+import React, { FC, useState } from 'react'
+import { ICategories } from './CategoriesDashboard'
 
 interface ICategoryAdd {
-  setOpenPopupAdd: Function;
-  setAllCategories: Function;
+    setOpenPopupAdd: Function
+    setAllCategories: Function
 }
 
-const CategoryAdd: FC<ICategoryAdd> = ({
-  setOpenPopupAdd,
-  setAllCategories,
-}) => {
-  const [category, setCategory] = useState<string>("");
-  const [loader, setLoader] = useState<boolean>(false);
+const CategoryAdd: FC<ICategoryAdd> = ({ setOpenPopupAdd, setAllCategories }) => {
+    const [category, setCategory] = useState<string>("")
+    const [loader, setLoader] = useState<boolean>(false)
 
-  const hendleAddCategory = async () => {
-    try {
-      setLoader(true);
-      if (category.length === 0) return alert("השדה לא יכול להיות ריק");
+    const hendleAddCategory = async () => {
+        try {
+            setLoader(true)
+            if (category.length === 0) return alert("השדה לא יכול להיות ריק")
 
-      const {
-        data: { continueWork, message, newCategory },
-      } = await axios.post("http://localhost:7575/categories/save-category", {
-        opt: category,
-      });
-      if (continueWork) {
-        alert(message);
-        return setAllCategories((categories: Array<ICategories>) => [
-          ...categories,
-          { _id: newCategory._id, opt: newCategory.opt },
-        ]);
-      }
-      return alert(message);
-    } catch (error) {
-      alert(error);
-    } finally {
-      setLoader(false);
+            const { data: { continueWork, message, newCategory } } = await axios.post("https://mayart-candles-api.vercel.app/categories/save-category", { opt: category })
+            if (continueWork) {
+                alert(message)
+                return setAllCategories((categories: Array<ICategories>) => [...categories, { _id: newCategory._id, opt: newCategory.opt }])
+            }
+            return alert(message)
+        } catch (error) {
+            alert(error)
+        } finally {
+            setLoader(false)
+        }
     }
-  };
 
-  const handleChangeInput = (ev: React.SyntheticEvent) => {
-    let target = ev.target as HTMLInputElement;
-    return setCategory(target.value);
-  };
+    const handleChangeInput = (ev: React.SyntheticEvent) => {
+        let target = ev.target as HTMLInputElement;
+        return setCategory(target.value);
+    };
 
-  return (
-    <div>
-      <h2>איזה סוג קטיגוריה את רוצה להוסיף?</h2>
-      <input type="text" onChange={handleChangeInput} />
-      <button
-        onClick={hendleAddCategory}
-        disabled={loader}
-        className={loader === true ? "form-btn_disable" : "form-btn_active"}
-      >
-        הוספה
-      </button>
-      <button
-        disabled={loader}
-        className={loader === true ? "form-btn_disable" : "form-btn_active"}
-        onClick={() => setOpenPopupAdd(false)}
-      >
-        ביטול
-      </button>
-    </div>
-  );
-};
+    return (
+        <div className='popupAdd'>
+            <h2 className='titlePopupAdd'>
+                איזה סוג קטיגוריה את רוצה להוסיף?
+            </h2>
+            <input id='addInput' type="text" onChange={handleChangeInput} />
+            <button
+                onClick={hendleAddCategory}
+                disabled={loader}
+                className={loader === true ? "form-btn_disable" : "form-btn_active"}
+            >הוספה</button>
+            <button
+                disabled={loader}
+                className={loader === true ? "form-btn_disable" : "form-btn_active"}
+            onClick={()=> setOpenPopupAdd(false)}
+            >ביטול</button>
+        </div>
+    )
+}
 
-export default CategoryAdd;
+export default CategoryAdd
