8a849eec299daf77389b6353d3b3ee3341940531
ShayFoyer <tfoyer@gmail.com>
2024-06-19 20:55:15 +0300

    payment

--- react/src/views/Dashboard/Payment/Payment.tsx
+++ react/src/views/Dashboard/Payment/Payment.tsx
@@ -1,46 +1,47 @@
 import axios from "axios";
-import { FC, Suspense } from "react";
+import { FC, Suspense, useState } from "react";
 import { Await, defer, useLoaderData } from "react-router-dom";
-import paymantDesc from './paymentDesc'
+import PaymentDesc from "./paymentDesc";
+// import paymantDesc from "./paymentDesc";
 
 interface IPayment {
-  _id: string
-  desc: string
+  _id: string;
+  desc: string;
 }
 const Payment: FC = () => {
-  const {payment}= useLoaderData() as {payment:IPayment};
+  const { payment } = useLoaderData() as { payment: IPayment };
+  const [paymentText, setPaymentText] = useState<string>(
+    " *לקוחות יקרים, שימו לב שבעת מעבר לתשלום יפתח צ’אט בWhatsapp שם תתבצע ההזמנה מולי. הפריטים שהוספתם לסל יופיעו לכן אין צורך לחזור על ההזמנה. תודה, מיה :)",
+  );
   return (
-    <Suspense fallback={<h1 className='no_data_text'>Loading...</h1>}>
+    <Suspense fallback={<h1 className="no_data_text">Loading...</h1>}>
       <Await resolve={payment}>
-    <section className='payment'>
-      <h1 className='paymentTitle'>
-      טקסט עמוד תשלום
-      </h1>
-      
-      {/*<paymantDesc id={Payment._id} desc={Payment.desc} />*/}
-        <p className='paymentPara'>
-        *לקוחות יקרים, שימו לב שבעת מעבר לתשלום יפתח צ’אט בWhatsapp שם תתבצע ההזמנה מולי. הפריטים שהוספתם לסל יופיעו לכן אין צורך לחזור על ההזמנה. תודה, מיה :)
-        </p>
-    </section>
-    </Await>
+        <section className="payment">
+          <h1 className="paymentTitle">טקסט עמוד תשלום</h1>
+
+          <PaymentDesc id={"123123"} desc={paymentText} />
+          {/* <p className="paymentPara">{paymentText}</p> */}
+        </section>
+      </Await>
     </Suspense>
-  )
-}
+  );
+};
 
-export default Payment
-
+export default Payment;
 
 const handleGetPayment = async () => {
   try {
-    const { data: { continueWork, paymentText } } = await axios.get("http://localhost:7575/")
-    if (continueWork) return paymentText
+    const {
+      data: { continueWork, paymentText },
+    } = await axios.get("http://localhost:7575/");
+    if (continueWork) return paymentText;
   } catch (error) {
-    return alert(error)
+    return alert(error);
   }
-}
+};
 
 export const paymentDashLoader = async () => {
   return defer({
-    payment: await handleGetPayment()
-  })
-}
+    payment: await handleGetPayment(),
+  });
+};
