b1423cab62a08ea5f064f6b3b67b19dfd564c379
Etan Joseph Heyman <etan@heyman.net>
2024-06-26 12:45:48 -0500

    ContactKnob sizes fixed

--- react/src/Components/ContactKnob/MailLogo.tsx
+++ react/src/Components/ContactKnob/MailLogo.tsx
@@ -3,24 +3,25 @@ import { Link } from "react-router-dom";
 
 const MailLogo = () => {
   return (
-    <Link to="mail.com">
+    <Link
+      className="flex size-[19.45px] items-center justify-center sm:size-[41.59px] lg:size-[34px]"
+      to="mail.com"
+    >
       <svg
-        className="h-[34px] w-[34px]"
-        width="34"
-        height="28"
+        className="h-[14.59px] w-[18.06px] sm:h-[31.19px] sm:w-[38.62px] lg:h-[25.5px] lg:w-[31.57px]"
         viewBox="0 0 34 28"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          d="M30.3572 1.25H3.64293C2.30166 1.25 1.21436 2.33732 1.21436 3.67857V24.3214C1.21436 25.6627 2.30166 26.75 3.64293 26.75H30.3572C31.6985 26.75 32.7858 25.6627 32.7858 24.3214V3.67857C32.7858 2.33732 31.6985 1.25 30.3572 1.25Z"
+          d="M30.3572 1.25H3.64287C2.3016 1.25 1.21429 2.33732 1.21429 3.67857V24.3214C1.21429 25.6627 2.3016 26.75 3.64287 26.75H30.3572C31.6985 26.75 32.7857 25.6627 32.7857 24.3214V3.67857C32.7857 2.33732 31.6985 1.25 30.3572 1.25Z"
           stroke="black"
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
         />
         <path
-          d="M1.21436 4.28571L15.4458 12.8446C15.8822 13.1008 16.4321 13.2411 17.0001 13.2411C17.568 13.2411 18.118 13.1008 18.5544 12.8446L32.7858 4.28571"
+          d="M1.21429 4.28571L15.4457 12.8446C15.8821 13.1008 16.4321 13.2411 17 13.2411C17.5679 13.2411 18.1179 13.1008 18.5543 12.8446L32.7857 4.28571"
           stroke="black"
           stroke-width="2"
           stroke-linecap="round"
--- react/src/Components/ContactKnob/index.tsx
+++ react/src/Components/ContactKnob/index.tsx
@@ -5,9 +5,9 @@ import MailLogo from "./MailLogo";
 
 const ContactKnob = () => {
   return (
-    <div className="fixed left-0 mt-56 flex h-[180px] w-[60px] flex-col items-center justify-around rounded-2xl bg-red-300 p-4">
+    <div className="fixed left-0 mt-56 flex w-fit flex-col items-center justify-around gap-[14.3px] rounded-2xl bg-red-300 px-[11.28px] py-[11.53px] sm:px-[25.6px] sm:py-[27px] lg:p-5">
+      <WhatsappLogo/>
       <InstagramLogo />
-      <WhatsappLogo />
       <MailLogo />
     </div>
   );
