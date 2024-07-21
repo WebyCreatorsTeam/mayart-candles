7518fdfdb8bb6ea0a4a8ea3720568d90b077bb71
Etan Joseph Heyman <etan@heyman.net>
2024-07-21 14:41:04 -0500

    Add gap between all candles and hero

--- react/src/views/index.tsx
+++ react/src/views/index.tsx
@@ -4,7 +4,7 @@ import AllCandles from "./Candles/AllCandles/AllCandles";
 
 const Home = () => {
   return (
-    <div className="scrollbar-none flex flex-col">
+    <div className="scrollbar-none flex gap-5 flex-col">
       <HeroImageSection />
       <AllCandles home />
     </div>
