6ec8bd9471c7595d966a1e5856b40967e7aa8c0c
Etan Joseph Heyman <etan@heyman.net>
2024-07-23 18:23:29 -0500

    Added filter and sort buttons, sort ready.

--- react/src/Components/ContactKnob/index.tsx
+++ react/src/Components/ContactKnob/index.tsx
@@ -12,7 +12,7 @@ const ContactKnob = () => {
     return link;
   };
   return (
-    <div className="fixed left-0 top-96 flex w-fit flex-col items-center justify-around gap-[14.3px] rounded-md bg-[#EDAFB8] px-[11.28px] py-[11.53px] sm:rounded-2xl sm:px-[25.6px] sm:py-[27px] xl:p-5">
+    <div className="fixed z-50 left-0 top-96 flex w-fit flex-col items-center justify-around gap-[14.3px] rounded-md bg-[#EDAFB8] px-[11.28px] py-[11.53px] sm:rounded-2xl sm:px-[25.6px] sm:py-[27px] xl:p-5">
       <WhatsappLogo link={findLink("whatsapp")} />
       <InstagramLogo link={findLink("instagram")} />
       <EmailLogo link={findLink("email")} />
--- react/src/views/Candles/AllCandles/AllCandles.tsx
+++ react/src/views/Candles/AllCandles/AllCandles.tsx
@@ -2,7 +2,16 @@ import React, { Suspense } from "react";
 import { Await, useLoaderData } from "react-router-dom";
 import { CandleType } from "../../../utils/types/candles";
 import Thumbnail from "../../../Components/Candles/Thumbnail";
-import FilterSort from "../../../Components/Candles/FilterSort";
+import { V6FilterSort } from "../../../Components/shadcn/components/v6-filter-sort";
+
+export enum TypeOfSortEnum {
+  AZ = "a-z",
+  ZA = "z-a",
+  LTH = "low-high",
+  HTL = "high-low",
+  NF = "new-old",
+  OF = "old-new",
+}
 
 const AllCandles = ({ home = false }: { home?: boolean }) => {
   const { candles, type, size } = useLoaderData() as {
@@ -10,20 +19,49 @@ const AllCandles = ({ home = false }: { home?: boolean }) => {
     type?: string;
     size?: string;
   };
-
+  const returnCurrentPrice = (candle: CandleType) =>
+    candle.salePrice ? candle.salePrice : candle.price;
+  // const [filterSet, setFilterSet] = React.useState({});
+  const [sortType, setSortType] = React.useState(TypeOfSortEnum.NF);
+  const sortedCandles = React.useMemo(() => {
+    switch (sortType) {
+      case TypeOfSortEnum.AZ:
+        return candles.sort((a, b) => (a.name > b.name ? 1 : -1));
+      case TypeOfSortEnum.ZA:
+        return candles.sort((a, b) => (a.name < b.name ? 1 : -1));
+      case TypeOfSortEnum.LTH:
+        return candles.sort((a, b) =>
+          returnCurrentPrice(a) > returnCurrentPrice(b) ? 1 : -1,
+        );
+      case TypeOfSortEnum.HTL:
+        return candles.sort((a, b) =>
+          returnCurrentPrice(a) < returnCurrentPrice(b) ? 1 : -1,
+        );
+      case TypeOfSortEnum.NF:
+        return Array(...candles).reverse();
+      case TypeOfSortEnum.OF:
+        return candles;
+    }
+  }, [candles, sortType]);
   return (
     <Suspense fallback={<h1 className="no_data_text">Loading...</h1>}>
       <Await resolve={candles}>
         <div
           id="all_candles_list"
-          className={`flex w-full relative flex-col items-center gap-10 px-5 pb-14 sm:gap-20 xl:px-[118.5px] xl:pb-[69px] ${home ? "" : "xl:mt-48"}`}
+          className={`relative flex w-full flex-col items-center gap-10 px-5 pb-14 sm:gap-20 xl:px-[118.5px] xl:pb-[69px] ${home ? "" : "xl:mt-48"}`}
         >
-          <h1 className="border-b-[0.5px] border-[#B0C4B1] px-[30px] py-[21.5px] text-xl font-semibold sm:px-[64.15px] sm:py-[46.345px] sm:text-[42.77px] sm:leading-[56.89px]  xl:border-none xl:p-0 xl:text-[64px] xl:leading-[85.13px]">
+          <h1 className="mb-10 border-b-[0.5px] border-[#B0C4B1] px-[30px] py-[21.5px] text-xl font-semibold sm:px-[64.15px] sm:py-[46.345px] sm:text-[42.77px] sm:leading-[56.89px]  xl:border-none xl:p-0 xl:text-[64px] xl:leading-[85.13px]">
             {type ? type : size ? size : "כל הנרות"}
           </h1>
-          <FilterSort home={home} />
-          <div className=" grid w-full grid-flow-row grid-cols-2 items-center justify-center justify-items-center gap-5 xl:grid-cols-3 xl:gap-[47px]">
-            {candles.map((candle) => (
+
+          <V6FilterSort
+            // filterSet={}
+            sortType={sortType}
+            // setFilterSet={}
+            setSortType={setSortType}
+          />
+          <div dir="rtl" className="grid w-full grid-flow-row grid-cols-2 items-center justify-center justify-items-center gap-5 xl:grid-cols-3 xl:gap-[47px]">
+            {sortedCandles.map((candle) => (
               <Thumbnail key={candle._id} candle={candle} />
             ))}
           </div>
