a4a35b38c268feb62d0a17a69134e9a86ac76990
Etan Joseph Heyman <etan@heyman.net>
2024-07-25 21:56:09 -0500

    ShoppingCart page responsive for laptop screen

--- react/src/views/ShoppingCart/CheckoutAndDisclaimer/CheckoutForm.tsx
+++ react/src/views/ShoppingCart/CheckoutAndDisclaimer/CheckoutForm.tsx
@@ -16,7 +16,7 @@ const CheckoutForm = () => {
     <Form
       // navigate={false}
       preventScrollReset
-      className="w-full px-6 pb-[146px] sm:px-[46.29px] sm:pb-[121.68px]"
+      className="w-full px-6 pb-[146px] sm:px-[46.29px] sm:pb-[121.68px] xl:max-2xl:px-72"
       action="/candles/list/shoppingCart"
       method="post"
     >
@@ -25,7 +25,7 @@ const CheckoutForm = () => {
           אנא מלאו את הפרטים ונחזור אליכם הקדם!
         </h1>
         <div className="flex w-full flex-col gap-[42px] sm:gap-[89.82px]">
-          <div className="flex w-full flex-col gap-[5.64px] sm:gap-[12.06px] sm:text-[32px] sm:leading-[42.56px]">
+          <div className="flex w-full flex-col gap-[5.64px] sm:gap-[12.06px] sm:text-[32px] sm:leading-[42.56px] xl:max-2xl:text-3xl">
             <label className="w-full" htmlFor="fullName">
               שם מלא
             </label>
@@ -45,9 +45,18 @@ const CheckoutForm = () => {
               name="telPhone"
               id="telPhone"
             />
+            <label className="w-full " htmlFor="phone">
+              דואר אלקטרוני
+            </label>
+            <input
+              className="w-full bg-[#F06CBB33] p-[4.959px] focus:outline-none  sm:px-[33.58px] sm:py-[25px]"
+              type="email"
+              name="email"
+              id="email"
+            />
           </div>
 
-          <div className="flex w-full flex-col  items-center gap-[15px] text-xl font-semibold leading-[26.6px] sm:gap-[32.08px] sm:text-[32px] sm:leading-[42.56px]">
+          <div className="flex w-full flex-col  items-center gap-[15px] text-xl font-semibold leading-[26.6px] sm:gap-[32.08px] sm:text-[32px] sm:leading-[42.56px] xl:max-2xl:text-3xl">
             <CheckoutButton candlesArrayTrue={candlesArrayTrue} />
             <Link
               className="w-full border-4 border-black p-[23px] text-center  transition-colors duration-300 hover:bg-black hover:text-white active:bg-black active:text-white sm:w-fit sm:px-[100px] sm:py-10"
