e9ceedfc854781ec57fc7fdaf75483c757f52ea7
Etan Joseph Heyman <etan@heyman.net>
2024-07-29 07:39:47 -0500

    resize empty array continue shopping button

--- react/src/Components/ListComponents/EmptyArrayPrompt.tsx
+++ react/src/Components/ListComponents/EmptyArrayPrompt.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const EmptyArrayPrompt = () => {
   return (
-    <div className="flex h-full flex-col items-center justify-center gap-10 text-center text-xl font-semibold leading-[26.6px]">
+    <div className="flex h-full flex-col items-center justify-center gap-10 px-6 sm:px-[46.29px] xl:max-2xl:px-72 text-center text-xl font-semibold leading-[26.6px]">
       <h1 className="h-full">עדיין לא נוספו פריטים</h1>
       <Link
         className="w-full border-4 border-black p-[23px]  transition-colors duration-300 hover:bg-black hover:text-white active:bg-black active:text-white sm:w-fit sm:px-[100px] sm:py-10"
